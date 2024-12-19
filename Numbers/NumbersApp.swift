38e05e7dd386bf05ae633503c1d51d1d2a132310
Alex German <alexgerman@gmail.com>
2024-12-19 18:18:21 -0300

    Initial Commit

---
+++ Numbers/NumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumbersApp.swift
+//  Numbers
+//
+//  Created by Alex Maggio on 19/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct NumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
